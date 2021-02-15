3c860692223a1f3d2b8f84fd08d13c7084fbcba5
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-02-14 18:33:05 -0800

    Initial Commit

---
+++ AnimatingPath002/AnimatingPath002App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingPath002App.swift
+//  AnimatingPath002
+//
+//  Created by Vadiraj Hippargi on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingPath002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
